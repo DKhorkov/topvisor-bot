0e0993f7278f33a29d476cd1f24233c264242088
DKhorkov <dkhorkov.dev@gmail.com>
2024-08-27 10:17:37 +0300

    updated logging system

--- src/logging_system/config.py
+++ src/logging_system/config.py
@@ -1,13 +1,18 @@
 import logging
 import os
+import sys
 from pathlib import Path
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 LOG_FILE: Path = Path(os.getcwd()).joinpath('logs.log')
+
+__file_handler: logging.FileHandler = logging.FileHandler(filename=LOG_FILE)
+__stdout_handler: logging.StreamHandler = logging.StreamHandler(stream=sys.stdout)
+__handlers: List[logging.Handler] = [__file_handler, __stdout_handler]
 
 LOGGING_CONFIG: Dict[str, Any] = {
     'level': logging.INFO,
     'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     'datefmt': '%Y-%m-%d %H:%M:%S',
-    'filename': LOG_FILE
+    'handlers': __handlers
 }
